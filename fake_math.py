c67dfd8fe39070fbf7624d0dfa0b13e984ab4274
Mavridiy <161286685+Mavridiy@users.noreply.github.com>
2024-08-30 16:21:22 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,8 @@
+
+def divide (a, b):
+    if b == 0:
+        return 'ошибка'
+        # print ('ошибка')
+    else:
+        result = a / b
+        return result
---
+++ true_math.py
@@ -0,0 +1,10 @@
+
+from math import inf
+
+def divide (a, b):
+    if b == 0:
+        result = inf
+        return result
+    else:
+        result = a / b
+        return result
